312ea0ef686bb0ad0431085c7057ce7ee480075d
Oliver Chamberlain <S179650@ucs.ac.uk>
2017-12-02 18:16:08 +0000

    Camera script iterated, it can now alter its tilt between two variables

--- Unity Files/Monster-miner/Assets/Scripts/System/CameraController.cs
+++ Unity Files/Monster-miner/Assets/Scripts/System/CameraController.cs
@@ -7,6 +7,9 @@ public class CameraController : MonoBehaviour
     private float panSpeed = 20f;
     [SerializeField]
     private float CamRotationSpeed = 5;
+    [SerializeField]
+    [Range(0,0.5f)]
+    private float zoomSpeed;
     [Header("Minimum and maximum values")]
     [SerializeField]
     private float camMinZoom;
@@ -18,28 +21,35 @@ public class CameraController : MonoBehaviour
     [Space]
     [SerializeField]
     private float CameraScreenBorder = 10;
+
+    [Space]
     [SerializeField]
-    private AnimationCurve curve;
+    private LayerMask floorLayerMask;
 
 
-
-    private float cam_yPos = 0;
-    [SerializeField]
     private float zoomDifferentialOver100;
 
+
+
+    private float expectedYPosition;
+
+    private Vector2 middleClickStart;
+
     [SerializeField]
-    [Range(0,0.5f)]
-    private float zoomSpeed;
+    private float mousePanBoxSize = 10f; // size of the box in PIXELS
 
     private void Start()
     {
         zoomDifferentialOver100 = (camMaxZoom - camMinZoom) / 100;
         zoomSpeed *= 100;
+        expectedYPosition = transform.position.y;
     }
-
+    float distFromFloor;
     void Update()
     {
         Vector3 CamPosition = transform.position;
+        Vector3 CamRotation = transform.localEulerAngles;
+
 
         #region mouseScroll
         if (!EventSystem.current.IsPointerOverGameObject())
@@ -48,16 +58,25 @@ public class CameraController : MonoBehaviour
 
             //if the mouse scroll is greater than 0 then we will increase new_zPos otherwise we will reduce it
             if (mouseScroll > 0)
+            {
                 //transform.Translate(new Vector3(0, -zoomDifferentialOver100 * zoomSpeed,0));
-                CamPosition += new Vector3(0, -zoomDifferentialOver100 * zoomSpeed, 0);
+                //CamPosition += new Vector3(0, -zoomDifferentialOver100 * zoomSpeed, 0);
+                expectedYPosition += -zoomDifferentialOver100 * zoomSpeed;
+            }
 
             else if (mouseScroll < 0)
+            {
                 //transform.Translate(new Vector3(0, zoomDifferentialOver100 * zoomSpeed, 0));
-                CamPosition += new Vector3(0, zoomDifferentialOver100 * zoomSpeed, 0);
+                //CamPosition += new Vector3(0, zoomDifferentialOver100 * zoomSpeed, 0);
+                expectedYPosition += zoomDifferentialOver100 * zoomSpeed;
+            }
 
 
         }
         #endregion
+
+
+
         #region camera movement
         //translation
         if (Input.GetKey(Keybinds.Instance.MoveCameraUp) )//|| Input.mousePosition.y >= Screen.height - CameraScreenBorder)
@@ -77,20 +96,67 @@ public class CameraController : MonoBehaviour
 
         //rotation
         if (Input.GetKey(Keybinds.Instance.RotateCameraLeft))
-            transform.Rotate((-transform.up * CamRotationSpeed));
+           CamRotation += -Vector3.up * CamRotationSpeed;
 
         if (Input.GetKey(Keybinds.Instance.RotateCameraRight))
-            transform.Rotate((transform.up * CamRotationSpeed));
+            CamRotation +=  Vector3.up * CamRotationSpeed;
 
+
+        if(Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            middleClickStart = Input.mousePosition;
+        }
+        //then get the mouse position while the mmb is still pressed
+        if(Input.GetKey(KeyCode.Mouse2))
+        {
+            //look up
+            if(Input.mousePosition.x > middleClickStart.x + mousePanBoxSize)
+            {
+               CamRotation += Vector3.up * CamRotationSpeed;
+            }
+            //look down
+            if(Input.mousePosition.x < middleClickStart.x - mousePanBoxSize)
+            {
+               CamRotation += -Vector3.up * CamRotationSpeed;
+            }
+            //look right
+            if(Input.mousePosition.y > middleClickStart.y + mousePanBoxSize)
+            {
+                transform.GetChild(0).localEulerAngles -= Vector3.right * (CamRotationSpeed /2);
+            }
+            //look left
+            if(Input.mousePosition.y < middleClickStart.y - mousePanBoxSize)
+            {
+                transform.GetChild(0).localEulerAngles -= -Vector3.right * (CamRotationSpeed/2);
+            }
+           
+
+        }
+        
         #endregion
 
 
         CamPosition.x = Mathf.Clamp(CamPosition.x, (-CameraBounds.x)/2, (CameraBounds.x)/2);
-        CamPosition.y = Mathf.Clamp(CamPosition.y, camMinZoom, camMaxZoom);
         CamPosition.z = Mathf.Clamp(CamPosition.z, (-CameraBounds.y)/2, (CameraBounds.y)/2);
+
+        //we need to make sure that the camera's position is at a static distance from the floor rather than a world space position
+        RaycastHit rayHit = new RaycastHit();
+        if(Physics.Raycast(CamPosition,Vector3.down, out rayHit, float.MaxValue,floorLayerMask))
+        {
+            distFromFloor = Vector3.Distance(CamPosition, rayHit.point);
+        }
+
+        float expectedDistFromFloor = CamPosition.y - distFromFloor;
+        CamPosition.y = expectedYPosition + expectedDistFromFloor;
+        CamPosition.y = Mathf.Clamp(CamPosition.y, camMinZoom + expectedDistFromFloor, camMaxZoom + expectedDistFromFloor);
+
+
 
         transform.position = CamPosition;
 
+        transform.GetChild(0).localEulerAngles = new Vector3(Mathf.Clamp(transform.GetChild(0).localEulerAngles.x, 20, 80),transform.GetChild(0).localEulerAngles.y, transform.GetChild(0).localEulerAngles.z);
+
+        transform.localEulerAngles = CamRotation;
 
 
     }
